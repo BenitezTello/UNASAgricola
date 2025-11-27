963c59e7914801f718e7adc37e6433c78d33a518
BenitezTello <carlos.benitez@unas.edu.pe>
2025-11-27 14:51:20 -0500

    Fix: mover .gitignore a raíz y remover .terraform del repo

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,6 +8,8 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
 
+  map_public_ip_on_launch = true       # <-- 🔥 ESTA ES LA SOLUCIÓN
+
   enable_dns_hostnames = true
   enable_dns_support   = true
 
